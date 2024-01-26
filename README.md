c8cfa050c07a71ff3ef5c8a9245d642eb4921ca3
Anshul Chauhan <anshulchauhan@outlook.com>
2024-01-26 23:29:19 +0530

    Refactor insertion sort

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Writing data structures & algorithms in Scala 2.13
 
 1. [Binary Search](./src/main/scala/example/BinarySearch.scala)
 1. [Bubble Sort](./src/main/scala/example/BubbleSort.scala)
-1. [Selection Sort](./src/test/scala/example/SelectionSortSpec.scala)
-1. [Insertion Sort](./src/test/scala/example/InsertionSortSpec.scala)
+1. [Selection Sort](./src/main/scala/example/SelectionSort.scala)
+1. [Insertion Sort](./src/main/scala/example/InsertionSort.scala)
--- src/main/scala/example/InsertionSort.scala
+++ src/main/scala/example/InsertionSort.scala
@@ -4,24 +4,14 @@ object InsertionSort {
   def insertionSort(array: Array[Int]): Array[Int] = {
     var arr = array;
 
-    // 1. temporarily remove element at index 1 (pin)
-    // 2. Begin comparing phase, compare the item left to the _pin_
-    // 3. if the item left of pin is greater than the item in pin, we move _it_ to the right
-    // 4. we keep going left until we reach the end, or we reach an element smaller than _pin_
-    // 5. We put the value in pin there
-    // 6. We start step 1, with index 2 and keep going until the end of the loop.
-
     for (i <- 1 until arr.size) {
-      // store value at i in a temp variable
       val temp = arr(i);
-      var j = i-1 // immediate value left of the _i_
-      while (j >= 0) {
-        if (arr(j) > temp){
-          arr(j+1) = arr(j);
-          arr(j) = temp;
-        }
+      var j = i-1;
+      while (j >= 0 && (arr(j) > temp)) {
+        arr(j+1) = arr(j);
         j -= 1;
       }
+      arr(j+1) = temp;
     }
 
     arr
